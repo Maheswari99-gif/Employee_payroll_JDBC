7f5a3c393c8f0cbc911f97db2d6650d05babb10a
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-11-02 18:36:17 +0530

    MaheswariKoyyani : Retrieve all functions

--- main/java/com/capgemini/employeepayrolljdbc/EmployeePayrollService.java
+++ main/java/com/capgemini/employeepayrolljdbc/EmployeePayrollService.java
@@ -12,6 +12,12 @@ public class EmployeePayrollService {
 	public enum statementType {
 		STATEMENT, PREPARED_STATEMENT
 	}
+	/**
+	 * 
+	 * @return
+	 * @throws DBServiceException
+	 * usecase2
+	 */
 
 	public List<EmployeePayrollData> viewEmployeePayroll() throws DBServiceException {
 		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
@@ -25,14 +31,8 @@ public class EmployeePayrollService {
 				String name = resultSet.getString(2);
 				double salary = resultSet.getDouble(3);
 				LocalDate start = resultSet.getDate(4).toLocalDate();
-				/*
-				 * double basic_pay = resultSet.getDouble(6); double deductions =
-				 * resultSet.getDouble(7); double taxable_pay = resultSet.getDouble(8); double
-				 * tax = resultSet.getDouble(9); double net_pay = resultSet.getDouble(10); int
-				 * comp_id = resultSet.getInt(11); String phn_no = resultSet.getString(12);
-				 * String address = resultSet.getString(13);
-				 */
-				empDataObj = new EmployeePayrollData(emp_id, name, salary, start);
+				String gender=resultSet.getString(5);
+				empDataObj = new EmployeePayrollData(emp_id, name, salary, start,gender);
 				employeePayrollList.add(empDataObj);
 			}
 		} catch (Exception e) {
@@ -40,6 +40,13 @@ public class EmployeePayrollService {
 		}
 		return employeePayrollList;
 	}
+	/**
+	 * 
+	 * @param name
+	 * @param salary
+	 * @return
+	 * usecase3
+	 */
 
 	public int updateSalary(String name, Double salary) {
 		String sqlQuery = String.format("UPDATE employee_payroll SET salary = %.2f WHERE NAME = '%s';", salary, name);
@@ -51,6 +58,15 @@ public class EmployeePayrollService {
 		}
 		return 0;
 	}
+	/**
+	 * 
+	 * @param name
+	 * @param salary
+	 * @param preparedStatement
+	 * @return
+	 * @throws DBServiceException
+	 * usecase4
+	 */
 
 	public int updateSalaryUsingPreparedStatement(String name, double salary, statementType preparedStatement)
 			throws DBServiceException {
@@ -78,6 +94,14 @@ public class EmployeePayrollService {
 				.filter(employeeObj -> ((employeeObj.getName()).equals(name))).findFirst().orElse(null);
 		return employee;
 	}
+	/**
+	 * 
+	 * @param startDate
+	 * @param endDate
+	 * @return
+	 * @throws DBServiceException
+	 * usecase5
+	 */
 	public List<EmployeePayrollData> viewEmployeePayrollByJoinDateRange(LocalDate startDate , LocalDate endDate) throws DBServiceException
 	{
 		List<EmployeePayrollData> employeePayrollListByStartDate = new ArrayList<>();
@@ -93,7 +117,8 @@ public class EmployeePayrollService {
 				String name = resultSet.getString(2);
 				double salary = resultSet.getDouble(3);
 				LocalDate start = resultSet.getDate(4).toLocalDate();
-				empDataObj = new EmployeePayrollData(emp_id, name,salary,start);
+				String gender=resultSet.getString(5);
+				empDataObj = new EmployeePayrollData(emp_id, name,salary,start,gender);
 				employeePayrollListByStartDate.add(empDataObj);
 			}
 		} catch (Exception e) {
@@ -101,5 +126,29 @@ public class EmployeePayrollService {
 		}
 		return employeePayrollListByStartDate;
 	}
+	/**
+	 * 
+	 * @param column
+	 * @param operation
+	 * @return
+	 * @throws DBServiceException
+	 * usecase6
+	 */
+	public Map<String,Double> viewEmployeeDataGroupedByGender(String column , String operation) throws DBServiceException
+	{
+		Map<String,Double> empDataByGender = new HashMap<>();
+		String query = String.format("select gender , %s(%s) from Employee_Payroll group by gender;" , operation , column);
+		try(Connection con = new JDBC().getConnection()) {
+			PreparedStatement preparedStatement = con.prepareStatement(query);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while(resultSet.next())
+			{
+				empDataByGender.put(resultSet.getString(1), resultSet.getDouble(2));
+			}
+		}catch (Exception e) {
+			throw new DBServiceException("SQL Exception", DBServiceExceptionType.SQL_EXCEPTION);
+		}
+		return empDataByGender;
+	}	
 }
 
--- test/java/com/capgemini/employeepayrolljdbc/EmployeePayrollTest.java
+++ test/java/com/capgemini/employeepayrolljdbc/EmployeePayrollTest.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,10 +15,14 @@ import org.junit.Test;
 public class EmployeePayrollTest {
 	static EmployeePayrollService serviceObj;
 	private List<EmployeePayrollData> employeeList = new ArrayList<>();
+	static Map<String, Double> empDataByGender;
+	static List<EmployeePayrollData> empPayrollList;
 
 	@BeforeClass
 	public static void setUp() {
 		serviceObj = new EmployeePayrollService();
+		empDataByGender = new HashMap<>();
+		empPayrollList = new ArrayList<>();
 	}
 
 	@Test
@@ -45,5 +51,38 @@ public class EmployeePayrollTest {
 		List<EmployeePayrollData> empPayrollList = serviceObj.viewEmployeePayrollByJoinDateRange(LocalDate.of(2018,02,01), LocalDate.now() );
 		assertEquals(3, empPayrollList.size());
 	}
+	@Test
+	public void givenEmployeeDB_WhenRetrievedSum_ShouldReturnSumGroupedByGender() throws DBServiceException {
+		empDataByGender = serviceObj.viewEmployeeDataGroupedByGender("salary" , "sum");
+		assertEquals(1550000, empDataByGender.get("M"), 0.0);
+		assertEquals(300000, empDataByGender.get("F"), 0.0);
+	}
 
+	@Test
+	public void givenEmployeeDB_WhenRetrievedAvg_ShouldReturnAvgByGroupedGender() throws DBServiceException {
+		empDataByGender = serviceObj.viewEmployeeDataGroupedByGender("salary" , "avg");
+		assertEquals(3100000, empDataByGender.get("M"), 0.0);
+		assertEquals(300000, empDataByGender.get("F"), 0.0);
+	}
+
+	@Test
+	public void givenEmployeeDB_WhenRetrievedMax_ShouldReturnMaxGroupedByGender() throws DBServiceException {
+		empDataByGender = serviceObj.viewEmployeeDataGroupedByGender("salary" , "max");
+		assertEquals(3000000, empDataByGender.get("M"), 0.0);
+		assertEquals(300000, empDataByGender.get("F"), 0.0);
+	}
+	
+	@Test
+	public void givenEmployeeDB_WhenRetrievedMin_ShouldReturnMinGroupedByGender() throws DBServiceException {
+		empDataByGender = serviceObj.viewEmployeeDataGroupedByGender("salary" , "min");
+		assertEquals(100000, empDataByGender.get("M"), 0.0);
+		assertEquals(300000, empDataByGender.get("F"), 0.0);
+	}
+
+	@Test
+	public void givenEmployeeDB_WhenRetrievedCount_ShouldReturnCountGroupedByGender() throws DBServiceException {
+		empDataByGender = serviceObj.viewEmployeeDataGroupedByGender("salary", "count");
+		assertEquals(2, empDataByGender.get("M"), 0.0);
+		assertEquals(1, empDataByGender.get("F"), 0.0);
+	}
 }
